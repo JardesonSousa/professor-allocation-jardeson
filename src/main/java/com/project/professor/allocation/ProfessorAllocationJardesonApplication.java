8b08e49a6d0b73bb95d3f5123148533e9b9abb42
Jardeson Augusto <jardesonsousa123@hotmail.com>
2022-02-17 00:52:58 -0300

    esquevi um nome

--- src/main/java/com/project/professor/allocation/ProfessorAllocationJardesonApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationJardesonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationJardesonApplication {
-
+//qualquer coisa
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationJardesonApplication.class, args);
 	}
