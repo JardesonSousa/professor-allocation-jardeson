96472e7499a720955df3b016948fdafdfd9c0678
Jardeson Augusto <jardesonsousa123@hotmail.com>
2022-02-21 17:04:07 -0300

    mapeamento das tabelas e chave primaria

--- src/main/java/com/project/professor/allocation/ProfessorAllocationJardesonApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationJardesonApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationJardesonApplication {
-//qualquer coisa
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationJardesonApplication.class, args);
 	}
 
 }
-// acrescentar comentario
+
