74c327d7405ce69229c14b11b77e20c8c3d0e48c
Jardeson Augusto <jardesonsousa123@hotmail.com>
2022-02-23 17:41:00 -0300

    att repository

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -1,5 +1,12 @@
 package com.project.professor.allocation.repository;
 
-public interface AllocationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.professor.allocation.entity.Allocation;
+
+@Repository
+
+public interface AllocationRepository extends JpaRepository<Allocation,Long> {
 
 }
