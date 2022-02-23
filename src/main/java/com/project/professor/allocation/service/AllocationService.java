544ee673850af4fe20175d7ed4aeaf6eedc0ad74
Jardeson Augusto <jardesonsousa123@hotmail.com>
2022-02-23 20:17:42 -0300

    add allocation service

---
+++ src/main/java/com/project/professor/allocation/service/AllocationService.java
@@ -0,0 +1,61 @@
+package com.project.professor.allocation.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.repository.AllocationRepository;
+
+@Service
+public class AllocationService {
+	
+	private  AllocationRepository allocationRepository;
+	
+	public AllocationService(AllocationRepository allocationRepository) {
+		super();
+		this.allocationRepository = allocationRepository;
+	}
+	//CRUD READ all
+	public List<Allocation> findAll()
+	{
+List<Allocation> allocations = allocationRepository.findAll();
+return allocations;
+  }
+	
+	//CRUD: by ID
+	public Allocation findById(Long id)
+	{
+Optional<Allocation> optional = allocationRepository.findById(id);
+Allocation allocation = optional.orElse(null);
+return allocation;
+	}
+	
+	//CRUD: CREATE
+	public Allocation create (Allocation allocation)
+	{
+		Allocation allocationNew = allocationRepository.save(allocation);
+	return allocationNew;
+	}
+	
+	//CRUD : UPDATE
+	public Allocation update(Allocation allocation)
+	{
+		Long id = allocation.getId();
+		if (id != null && allocationRepository.existsById(id))
+		{
+			Allocation allocationNew = allocationRepository.save(allocation);
+			return allocationNew;
+		}
+		else
+		{
+			return null;
+		}
+	}
+}
+
+
+
+
+
